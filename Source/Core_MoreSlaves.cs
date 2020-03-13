eef2b23eb85f4dac5ec9d54636c3433d0ac48050
Syrchalis <41159904+Syrchalis@users.noreply.github.com>
2020-03-14 00:23:41 +0100

    Add files via upload

---
+++ Source/Core_MoreSlaves.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using System.Reflection;
+using UnityEngine;
+
+namespace Syrchalis_MoreSlaves
+{
+    public class MoreSlaves : Mod
+    {
+        public static MoreSlavesSettings settings;
+
+        public MoreSlaves(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<MoreSlavesSettings>();
+        }
+
+        public override string SettingsCategory() => "MoreSlavesSettingsLabel".Translate();
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            checked
+            {
+                Listing_Standard listing_Standard = new Listing_Standard();
+                listing_Standard.Begin(inRect);
+                listing_Standard.Label("MoreSlavesSettingSlaveCount".Translate(), tooltip: "MoreSlavesSettingsSlaveCountTooltip".Translate());
+                listing_Standard.IntRange(ref MoreSlavesSettings.slaveCount, 0, 20);
+                listing_Standard.End();
+                settings.Write();
+            }
+        }
+
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+        }
+    }
+}
---
+++ Source/ModSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using System.Reflection;
+using UnityEngine;
+
+namespace Syrchalis_MoreSlaves
+{
+    public class MoreSlavesSettings : ModSettings
+    {
+        public static IntRange slaveCount = new IntRange(3, 5);
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look<IntRange>(ref slaveCount, "slaveCount", new IntRange(3, 5) , true);
+        }
+    }
+}
